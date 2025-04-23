a5a7b097da6ff882bbe4a7b892c70ce345fd42a1
LeeBingler <binglerlee@gmail.com>
2025-04-23 19:10:38 +0200

    bool for parser instead of int

--- src/main.c
+++ src/main.c
@@ -9,26 +9,25 @@ static void print_helper(void) {
     printf("Usage: %s <rom_file>\n", NAME_EXEC);
 }
 
-
-static int parser_args(int argc, char **argv) {
+static bool parser_args(int argc, char **argv) {
     for (int i = 0; i < argc; i++) {
         if (strcmp(argv[i], "--help") == 0) {
             print_helper();
-            return 1;
+            return false;
         }
     }
 
     if (argc != 2) {
         fprintf(stderr, "Missing argument: missing <rom_file>\n");
         fprintf(stderr, "Try '%s --help' for more information.\n", NAME_EXEC);
-        return 1;
+        return false;
     }
 
-    return 0;
+    return true;
 }
 
 int main(int argc, char **argv) {
-    if (parser_args(argc, argv)) return EXIT_FAILURE;
+    if (!parser_args(argc, argv)) return EXIT_FAILURE;
 
     if(!load_cart(argv[1])) return EXIT_FAILURE;
 
