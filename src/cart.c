51c3c08a234a31b94ce6b867fd6afc170e8c25a9
LeeBingler <binglerlee@gmail.com>
2025-04-23 19:10:17 +0200

    remove printf debug

--- src/cart.c
+++ src/cart.c
@@ -145,8 +145,6 @@ bool load_cart(char *cart_name) {
     strncpy(ctx.filename, cart_name, sizeof(ctx.filename) - 1);
     ctx.filename[sizeof(ctx.filename) - 1] = '\0';
 
-    printf("Opened: %s\n", ctx.filename);
-
     fseek(fd, 0, SEEK_END);
     ctx.rom_size = ftell(fd);
     rewind(fd);
@@ -169,21 +167,11 @@ bool load_cart(char *cart_name) {
     ctx.header = (rom_header *)(ctx.rom_data + 0x100);
     ctx.header->title[15] = '\0';
 
-    printf("Cartridge Loaded:\n");
-    printf("\t Title    : %s\n", ctx.header->title);
-    printf("\t Type     : %2.2X (%s)\n", ctx.header->cartridge_type, get_cart_romtype_name());
-    printf("\t ROM Size : %d KB\n", 32 << ctx.header->rom_size);
-    printf("\t RAM Size : %2.2X\n", ctx.header->ram_size);
-    printf("\t LIC Code : %2.2X (%s)\n", ctx.header->old_licence_code, cart_lic_name());
-    printf("\t ROM Vers : %2.2X\n", ctx.header->rom_version);
-
     // Checksum test (https://gbdev.io/pandocs/The_Cartridge_Header.html#014d--header-checksum)
     u16 checksum = 0;
     for (u16 address = 0x0134; address <= 0x014c; address++) {
         checksum = checksum - ctx.rom_data[address] - 1;
     }
-
-    printf("\t Checksum : %2.2X (%s)\n", ctx.header->checksum, (checksum & 0xFF) ? "PASSED" : "FAILED");
 
     return true;
 }
